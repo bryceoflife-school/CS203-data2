69aeb6bede62b64528992d1a8e7b20fab98aa44f
Bryce Daniel <brycerdaniel@gmail.com>
2014-11-05 22:48:38 -0500

    Javadoc Madness
    MOAR OF THIS THAT AND THE OTHER THING

--- MultiSet/src/multiset/Bag.java
+++ MultiSet/src/multiset/Bag.java
@@ -17,34 +17,40 @@ public interface Bag<D extends Comparable> extends Sequenced<D> {
     public int cardinality();
 
     /**
-     * This method returns true if the multi-set (Bag) is empty. 
+     * This method returns true if the multi-set (Bag) is empty/
+     * Example: {A, B, C}.isEmptyHuh() = false /
+     * Example: {}.isEmptyHuh() = true
      * @return A boolean
      */
     public boolean isEmptyHuh();
 
     /**
-     * A method that returns true if a given element is included in multi-set (Bag)
+     * A method that returns true if a given element is included in multi-set (Bag) / 
+     * Example: {A, B, C}.member(B) = true 
      * @param elt a generic element of the multi-set (Bag)
      * @return A boolean
      */
     public boolean member(D elt);
 
     /**
-     * A method that returns the number of times an element appears in the multi-set (Bag)
+     * A method that returns the number of times an element appears in the multi-set (Bag) / 
+     * Example: {A, B, B, B, C}.getCount(B) = 3
      * @param elt a generic element of the multi-set (Bag)
      * @return An Integer representing the number of times
      */
     public int getCount(D elt);
 
     /**
-     * A method that adds an element to the multi-set (Bag) and returns a new bag
+     * A method that adds an element to the multi-set (Bag) and returns a new bag / 
+     * Example: {A, B, C}.add(D) = {A, B, C, D}.
      * @param elt a generic element of the multi-set (Bag)
      * @return A Bag
      */
     public Bag<D> add(D elt);
 
     /**
-     * A method that adds an element n number of times to the multi-set (Bag) and returns a new bag
+     * A method that adds an element n number of times to the multi-set (Bag) and returns a new bag / 
+     * Example: {A, B, C}.addN(D, 3) = {A, B, C, D, D, D}.
      * @param elt a generic element of the multi-set (Bag)
      * @param n the number of times to add the element
      * @return A Bag
@@ -52,14 +58,16 @@ public interface Bag<D extends Comparable> extends Sequenced<D> {
     public Bag<D> addN(D elt, int n);
 
     /**
-     * A method that removes an element from the multi-set (Bag) and returns a new bag
+     * A method that removes an element from the multi-set (Bag) and returns a new bag / 
+     * Example: {A, B, C}.remove(C) = {A, B}.
      * @param elt a generic element of the multi-set (Bag)
      * @return A Bag
      */
     public Bag<D> remove(D elt);
 
     /**
-     * A method that removes an element from the multi-set (Bag) n times and returns a new bag 
+     * A method that removes an element from the multi-set (Bag) n times and returns a new bag / 
+     * Example: {A, B, C, C, C}.removeN(C, 2) = {A, B, C}.
      * @param elt a generic element of the multi-set (Bag)
      * @param n the number of times to remove the element
      * @return A Bag
@@ -67,27 +75,31 @@ public interface Bag<D extends Comparable> extends Sequenced<D> {
     public Bag<D> removeN(D elt, int n);
 
     /**
-     * A method that removes all occurrences of an element from the multi-set and returns a new bag
+     * A method that removes all occurrences of an element from the multi-set and returns a new bag / 
+     * Example: {A, A, A, A, B, C}.removeAll(A) = {B, C}.
      * @param elt a generic element of the multi-set (Bag)
      * @return A Bag
      */
     public Bag<D> removeAll(D elt);
 
     /**
-     * A method that returns the union of two multi-sets (Bags)
+     * A method that returns the union of two multi-sets (Bags) /
+     * Example: {A, B, C}.union({D, E, F}) = {A, B, C, D, E, F}.
      * @param u A Bag
      * @return A Bag
      */
     public Bag<D> union(Bag<D> u);
 
     /**
-     * A method that returns the intersection of two multi-sets (Bags)
+     * A method that returns the intersection of two multi-sets (Bags) / 
+     * Example: {A, B, C}.inter({C, D, E}) = {C}.
      * @param u A Bag
      * @return A Bag
      */
     public Bag<D> inter(Bag<D> u);
 
     /**
+     * Example: {A, B, C}.diff({C, E, F}) = {A, B, E, F}.
      * A method that returns the difference of two multi-sets (Bags)
      * @param u A Bag
      * @return A Bag
@@ -95,14 +107,17 @@ public interface Bag<D extends Comparable> extends Sequenced<D> {
     public Bag<D> diff(Bag<D> u);
 
     /**
-     * A method that returns true if two multi-sets (Bags) are equal
+     * A method that returns true if two multi-sets (Bags) are equal /
+     * Example: {A, B, C}.equal({D, E, F}) = false /
+     * Example: {A, B, C}.equal({A, B, C}) = true
      * @param u A Bag
      * @return A boolean
      */
     public boolean equal(Bag<D> u);
 
     /**
-     * A method that returns true if set u is a subset of the multi-set (Bag)
+     * A method that returns true if set u is a subset of the multi-set (Bag) /
+     * Example: {A, B, C}.subset({A, B}) = true
      * @param u A Bag
      * @return A boolean
      */
